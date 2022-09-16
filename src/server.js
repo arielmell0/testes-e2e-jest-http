21079ca88921379e17956a7f241220333cee3340
arielmell0 <arielmello@rede.ulbra.br>
2022-09-15 23:31:13 -0300

    feat: adicionando features aos nossos metodos http

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
+import { once } from 'events';
 import { createServer } from 'http';
 
+const Database = new Map()
+function respondJSON(data, response) {
+    return response(JSON.stringify(data))
+}
+
 async function handler(request, response) {
-    response.end('hello world')
+    const { method } = request
+
+    if(method = 'GET') {
+        return respondJSON([...Database.values()], response)
+    }
+
+    if(method === 'POST') {
+        const body = JSON.parse(await once(request, 'data'))
+        console.log('recebido', body)
+        return respondJSON({ ok: 1 })
+    }
+
+    if(method === 'DELETE') {
+        
+        return
+    }
 }
 
 export default createServer(handler)
