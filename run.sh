dc83a2ef9822d386085b45d7cff657ebc62acf2e
arielmell0 <arielmello@rede.ulbra.br>
2022-09-16 21:24:20 -0300

    feat: metodo get, post e delete criados e com suas primeiras funcionalidades

---
+++ run.sh
@@ -0,0 +1,8 @@
+# GET
+curl http://localhost:3000
+
+# POST
+curl -X POST --data '{"nome": "Ariel Oliveira de Mello"}' http://localhost:3000
+
+# DELETE
+curl -X DELETE http://localhost:3000
--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
-import { once } from 'events';
-import { createServer } from 'http';
+import { createServer } from 'http'
+import { once } from 'events'
+import { randomUUID } from 'crypto'
 
 const Database = new Map()
 function respondJSON(data, response) {
-    return response(JSON.stringify(data))
+    return response.end(JSON.stringify(data))
 }
 
 async function handler(request, response) {
     const { method } = request
 
-    if(method = 'GET') {
+    if(method === 'GET') {
         return respondJSON([...Database.values()], response)
     }
 
     if(method === 'POST') {
         const body = JSON.parse(await once(request, 'data'))
         console.log('recebido', body)
-        return respondJSON({ ok: 1 })
+        const id = randomUUID()
+        Database.set(id, body)
+        return respondJSON({ ok: 1 }, response)
     }
 
     if(method === 'DELETE') {
         
-        return
+        return respondJSON({ ok: 1 }, response)
     }
 }
 
