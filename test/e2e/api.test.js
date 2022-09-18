e065496c139198a6965900cd2ce579531f671896
arielmell0 <arielmello@rede.ulbra.br>
2022-09-18 20:04:47 -0300

    feat: instalacao do supertest e rota get testada

--- test/e2e/api.test.js
+++ test/e2e/api.test.js
@@ -5,8 +5,18 @@ import {
     describe  
 } from '@jest/globals'
 
+import superTest from 'supertest'
+import Server from '../../src/server'
+
 describe('API E2E Test Suite', () => {
-    test.todo('GET / - should return an array')
+    test('GET / - should return an array', async () => {
+        const response = await superTest(Server)
+        .get('/')
+        const data = JSON.parse(response.text)
+        expect(data).toBeInstanceOf(Array)
+        console.log('text', response.text)
+    })
+
     test.todo('POST / - should save an item and return ok')
     test.todo('DELETE / - should delete all items and return ok')
 })
